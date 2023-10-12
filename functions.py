23e1f2fcdfc4c165b168d3639f1d021a3917e377
MicMaus <michael.yasynskyi@gmail.com>
2023-10-12 12:40:35 +0200

    12/10 12:40

--- functions.py
+++ functions.py
@@ -1,177 +1,11 @@
 from collections.abc import Iterator
 from error_handl_decorator import error_handling_decorator
 from error_handl_decorator import CustomError
-from collections import UserDict
-import re
-import datetime
-import pickle
-
-# classes
-class Field:
-    def __init__(self, value):
-        self._value = None
-        self.value = value
-    
-    def __str__(self):
-        return self.value
-
-    @property
-    def value(self) -> str:
-        return self._value
-
-
-class Name(Field):
-    @Field.value.setter
-    def value(self, new_value: str):
-        if new_value.isalpha():
-            self._value = new_value
-        else:
-            raise CustomError("please provide valid name")
-
-
-class Phone(Field):
-    def __init__(self, value):
-        super().__init__(str(value))
-    
-    @Field.value.setter
-    def value(self, new_value: str):
-        if new_value.isdigit() or new_value==None:
-            self._value = new_value
-        else:
-            raise CustomError("phone number should contain digits only")
-
-
-class Birthday(Field):
-    def __init__(self, value): 
-        super().__init__(value)
-    
-    @Field.value.setter
-    def value(self, new_value: str):
-        # check of birthday format
-        if not re.match(r'\d{4}-\d{2}-\d{2}', new_value):
-            raise CustomError("please enter a valid birthdate in the format YYYY-MM-DD")
-
-        # convert to datetime format
-        try:
-            date_value = datetime.datetime.strptime(new_value, "%Y-%m-%d")
-        except:
-            raise CustomError("please enter a valid birthdate in the format YYYY-MM-DD")
-        
-        # check the date correctness
-        if not (1900 <= date_value.year <= datetime.date.today().year and
-                    1 <= date_value.month <= 12 and
-                    1 <= date_value.day <= 31):
-                raise CustomError("please enter a valid birthdate in the format YYYY-MM-DD")
-        dob = date_value.date()
-        self._value = dob
-    
-
-class Email(Field):
-    def __init__(self, value):
-        super().__init__(str(value))
-    
-    @Field.value.setter
-    def value(self, new_value: str):
-        if not re.match(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', email):
-            return CustomError("please provide an email in the correct format")
-        self._value = new_value
-
-
-class Address(Field):
-    def __init__(self, value):
-        super().__init__(str(value))
-
-    @Field.value.setter
-    def value(self, new_value: str):
-        self._value = new_value
-
-
-class Note(Field):
-    def __init__(self, value):
-        super().__init__(str(value))
-
-    @Field.value.setter
-    def value(self, new_value: str):
-        self._value = new_value
-
-
-class Record:
-    def __init__(self, name, phone=None, birthday=None, email=None, address=None, note=None): 
-        self.name = Name(value=name)
-        self.phones = []
-        if phone:
-            self.add_new_phone(phone)
-        if birthday:
-            self.birthday = Birthday(value=birthday)
-        if email:
-            self.email = Email(value=email)
-        if address:
-            self.address = Address(value=address)
-        if note:
-            self.note = Note(value=note)
-
-    def days_to_birthday(self):
-        current_date = datetime.date.today()
-        next_birthday = datetime.date(current_date.year, self.birthday.value.month, self.birthday.value.day)
-        
-        if current_date > next_birthday:
-            next_birthday = datetime.date(current_date.year + 1, self.birthday.value.month, self.birthday.value.day)
-            
-        days_until_birthday = (next_birthday - current_date).days
-
-        return days_until_birthday
-
-    def add_new_phone(self, phone): 
-        self.phones.append(Phone(value=phone))
-
-    def amend_phone(self, name, new_phone, old_phone): 
-        phone_found = False
-        for stored_phone in self.phones.copy():
-            if str(stored_phone) == old_phone:
-                self.phones.remove(stored_phone)
-                self.add_new_phone(new_phone)
-                phone_found = True
-
-        if not phone_found:
-            raise CustomError("phone number was not found")
-
-    def remove_phone(self, phone):
-        phone_found = False
-        for stored_phone in self.phones.copy():
-            if str(stored_phone) == phone:
-                self.phones.remove(stored_phone)
-                phone_found = True
-
-        if not phone_found:
-            raise CustomError("phone number was not found")
-
-
-phone_book_file = r'./phone_book'
-
-
-class AddressBook(UserDict):
-    def __init__(self):
-        super().__init__()
-        try:
-            with open(phone_book_file, "rb") as fh:
-                deserialized_book = pickle.load(fh)
-            self.data = deserialized_book
-        except:
-            pass
-
-    def add_record(self, record: Record):
-        self.data[record.name.value] = record
-
-    def iterator(self, n): # n-number of records per page. is defined in show_page function
-        contacts_per_page = n
-        contacts = list(self.data.keys())
-        index = 0
-
-        while index < len(contacts):
-            yield contacts[index:index + contacts_per_page]
-            index += contacts_per_page
-
-phone_book = AddressBook()
+#from collections import UserDict
+# import re
+# import datetime
+# import pickle
+from classes import *
 
 
 # parser
@@ -196,12 +30,9 @@ def parse_input(user_input):
             
             elif func == change_phone:
                 name = input('please provide a contact name: ')
-                new_phone_number = input('please provide the new phone number: ') 
                 old_phone_number = input('please provide the phone number to be replaced: ')
+                new_phone_number = input('please provide the new phone number: ') 
                 return func(name, new_phone_number, old_phone_number)
-            
-            elif func == show_all:
-                return func()
             
             elif func == show_page:
                 page = input('please provide the page to display: ')
@@ -220,9 +51,6 @@ def parse_input(user_input):
                 
                 return func(name, info_to_delete)
             
-            elif func == hello:
-                return func()
-            
             elif func == search:
                 search_word = input('please provide a search request: ')
                 return func(search_word)
@@ -230,9 +58,11 @@ def parse_input(user_input):
             elif func == dtb:
                 name = input('please provide a contact name: ')
                 birth_date = input('please provide a bithday in a format YYYY-MM-DD: ')
-                return func(name, birth_date) 
+                return func(name, birth_date)
+            
+            else:  #run func which don't need args. eg.hello, help, show all
+                return func()
 
-        
     raise CustomError("please provide a valid command")
 
 
@@ -422,11 +252,19 @@ def dtb(name,notused=None, notused2=None, notused3=None):
             raise CustomError ("no birthday recorded")
     return record.days_to_birthday()
 
+def help():
+    try:
+        with open(r'./help.txt', 'r') as file:
+            file_content = file.read()
+        return file_content
+    except FileNotFoundError:
+        raise CustomError("File not found") 
+
 
 commands = {
     "add": add_contact,
     "contact": show_contact,
-    "change": change_phone,
+    "change phone": change_phone,
     "show all": show_all,
     "page": show_page,
     "remove field": remove_info,
@@ -434,5 +272,6 @@ commands = {
     "hello": hello,
     "search": search,
     "dtb": dtb,
+    "help": help,
 }
 
